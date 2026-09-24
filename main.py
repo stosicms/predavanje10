9f79e1e151a17ae0c1954eb5e6e1134b29c4a321
Stosicms <milos.stosic.sud@gmail.com>
2026-09-24 11:47:52 +0200

    Renames json and move to folder data

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import json
 
-with open("data.json", 'r') as file:
+with open("data/user.json", 'r') as file:
     data = json.load(file)
     data.append({
         "name" : "Jelena Mitrovic",
@@ -9,5 +9,5 @@ with open("data.json", 'r') as file:
         "gender" : "female"
     })
 
-with open("data.json", 'w') as file:
+with open("data/user.json", 'w') as file:
     json.dump(data, file, indent=4)
